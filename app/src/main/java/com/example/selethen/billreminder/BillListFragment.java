4e33f1d4741cf47a9c08104dc828cfe2bea14f70
Lukasz Parulski <lukaszparulski@wp.pl>
2017-09-02 16:11:34 +0200

    Adding Fragments

---
+++ app/src/main/java/com/example/selethen/billreminder/BillListFragment.java
@@ -0,0 +1,19 @@
+package com.example.selethen.billreminder;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BillListFragment extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        this.view = inflater.inflate(R.layout.bill_list_layout, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/selethen/billreminder/MenuFragment.java
@@ -0,0 +1,29 @@
+package com.example.selethen.billreminder;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MenuFragment extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.menu_layout, container, false);
+
+        Button buttonBills = (Button) view.findViewById(R.id.bills);
+        buttonBills.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.content_main, new BillListFragment()).addToBackStack("BillListFragment").commit();
+            }
+        });
+
+        return view;
+    }
+}
